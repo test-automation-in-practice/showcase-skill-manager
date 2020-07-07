8fbd05a4ec709e5ca4f86fc62acf8afcf2d5191c
Stefan Ludwig <stefan.ludwig@slu-it.de>
2020-07-07 23:56:43 +0200

    remove Spring Security as long as the security concept is not yet defined

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,6 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-hateoas")
-    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
