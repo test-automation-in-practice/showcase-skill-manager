fec3f38cacab5430ade6cdd113c2fcc205919c84
Stefan Ludwig <stefan.ludwig@slu-it.de>
2020-07-22 18:57:20 +0200

    added contract and test for pattern matching validation constraint

---
+++ src/test/kotlin/skillmanagement/test/contracts/MatchesPatternStringTypeContract.kt
@@ -0,0 +1,17 @@
+package skillmanagement.test.contracts
+
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
+import org.junit.jupiter.api.assertThrows
+import skillmanagement.domain.ValidationException
+
+interface MatchesPatternStringTypeContract : StringTypeContract {
+
+    val invalidExampleValues: Iterable<String>
+
+    @TestFactory
+    fun `examples of invalid values`(): List<DynamicTest> = invalidExampleValues.map { value ->
+        DynamicTest.dynamicTest(value) { assertThrows<ValidationException> { createInstance(value) } }
+    }
+
+}
