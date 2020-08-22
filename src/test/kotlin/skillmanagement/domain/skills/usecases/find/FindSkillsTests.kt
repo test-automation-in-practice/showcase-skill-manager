56203b70133024b715115f649e4e2637704c0e63
Stefan Ludwig <stefan.ludwig@slu-it.de>
2020-08-22 12:39:39 +0200

    added missing test

--- src/test/kotlin/skillmanagement/domain/skills/usecases/find/FindSkillsTests.kt
+++ src/test/kotlin/skillmanagement/domain/skills/usecases/find/FindSkillsTests.kt
@@ -1,7 +1,11 @@
 package skillmanagement.domain.skills.usecases.find
 
+import io.kotlintest.shouldBe
+import io.mockk.every
 import io.mockk.mockk
 import org.junit.jupiter.api.Test
+import skillmanagement.domain.skills.model.skill_kotlin
+import skillmanagement.domain.skills.model.skill_python
 import skillmanagement.domain.skills.searchindex.SkillSearchIndex
 import skillmanagement.domain.skills.usecases.get.GetSkillFromDataStore
 import skillmanagement.test.UnitTest
@@ -15,8 +19,17 @@ internal class FindSkillsTests {
     val findSkills = FindSkills(findAllSkillsInDataStore, getSkillFromDataStore, searchIndex)
 
     @Test
-    fun `abc def`() {
-        TODO()
+    fun `NoOpQuery just returns all skills from database`() {
+        every { findAllSkillsInDataStore() } returns listOf(skill_kotlin, skill_python)
+        findSkills(NoOpQuery) shouldBe listOf(skill_kotlin, skill_python)
+    }
+
+    @Test
+    fun `SkillsMatchingQuery gets IDs from search index and then corresponding skills from database`() {
+        val query = SkillsMatchingQuery("kotlin")
+        every { searchIndex.query("kotlin") } returns listOf(skill_kotlin.id)
+        every { getSkillFromDataStore(listOf(skill_kotlin.id)) } returns mapOf(skill_kotlin.id to skill_kotlin)
+        findSkills(query) shouldBe listOf(skill_kotlin)
     }
 
 }
