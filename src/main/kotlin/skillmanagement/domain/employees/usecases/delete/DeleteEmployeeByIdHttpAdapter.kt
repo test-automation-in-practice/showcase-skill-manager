a0704d67c05fecc6bb2c67cc01f111b52a840f2e
Stefan Ludwig <stefan.ludwig@slu-it.de>
2020-09-26 22:22:49 +0200

    changed response codes for all delete operations to '204 No Content' if there were no exceptions

--- src/main/kotlin/skillmanagement/domain/employees/usecases/delete/DeleteEmployeeByIdHttpAdapter.kt
+++ src/main/kotlin/skillmanagement/domain/employees/usecases/delete/DeleteEmployeeByIdHttpAdapter.kt
@@ -1,6 +1,6 @@
 package skillmanagement.domain.employees.usecases.delete
 
-import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatus.NO_CONTENT
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -15,7 +15,7 @@ class DeleteEmployeeByIdHttpAdapter(
 ) {
 
     @DeleteMapping
-    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ResponseStatus(NO_CONTENT)
     fun delete(@PathVariable id: UUID) {
         deleteEmployeeById(id)
     }
--- src/main/kotlin/skillmanagement/domain/employees/usecases/skillknowledge/delete/DeleteSkillKnowledgeOfEmployeeHttpAdapter.kt
+++ src/main/kotlin/skillmanagement/domain/employees/usecases/skillknowledge/delete/DeleteSkillKnowledgeOfEmployeeHttpAdapter.kt
@@ -1,14 +1,12 @@
 package skillmanagement.domain.employees.usecases.skillknowledge.delete
 
 import mu.KotlinLogging.logger
-import org.springframework.http.HttpStatus.NO_CONTENT
 import org.springframework.http.ResponseEntity
-import org.springframework.http.ResponseEntity.notFound
+import org.springframework.http.ResponseEntity.noContent
 import org.springframework.http.ResponseEntity.ok
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
 import skillmanagement.common.stereotypes.HttpAdapter
 import skillmanagement.domain.employees.model.EmployeeResource
 import skillmanagement.domain.employees.model.toResource
@@ -26,13 +24,12 @@ class DeleteSkillKnowledgeOfEmployeeHttpAdapter(
     private val log = logger {}
 
     @DeleteMapping
-    @ResponseStatus(NO_CONTENT)
     fun delete(@PathVariable employeeId: UUID, @PathVariable skillId: UUID): ResponseEntity<EmployeeResource> {
         log.info { "Deleting knowledge of skill [$skillId] of employee [$employeeId]" }
         val result = deleteSkillKnowledgeOfEmployee(employeeId, skillId)
         log.info { "Result: $result" }
         return when (result) {
-            EmployeeNotFound, SkillKnowledgeNotFound -> notFound().build()
+            EmployeeNotFound, SkillKnowledgeNotFound -> noContent().build()
             is SuccessfullyDeletedSkillKnowledge -> ok(result.employee.toResource())
         }
     }
