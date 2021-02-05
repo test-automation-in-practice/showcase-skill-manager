ef133ad1566ab848ab86d3f0000364e29cdfa939
Stefan Ludwig <stefan.ludwig@novatec-gmbh.de>
2021-02-05 11:39:19 +0100

    dependency update

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,11 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED
 import org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED
 
 plugins {
-    id("org.springframework.boot") version "2.3.4.RELEASE"
-    id("io.spring.dependency-management") version "1.0.10.RELEASE"
-    id("org.asciidoctor.jvm.convert") version "3.1.0"
-    kotlin("jvm") version "1.4.10"
-    kotlin("plugin.spring") version "1.4.10"
+    id("org.springframework.boot") version "2.3.8.RELEASE"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("org.asciidoctor.jvm.convert") version "3.3.0"
+    kotlin("jvm") version "1.4.21"
+    kotlin("plugin.spring") version "1.4.21"
 }
 
 java.sourceCompatibility = JavaVersion.VERSION_11
@@ -32,7 +32,7 @@ dependencies {
     implementation("org.flywaydb:flyway-core")
 
     implementation("com.github.java-json-tools:json-patch:1.13")
-    implementation("io.github.microutils:kotlin-logging:1.8.3")
+    implementation("io.github.microutils:kotlin-logging:2.0.4")
     implementation("org.elasticsearch.client:elasticsearch-rest-high-level-client")
 
     runtimeOnly("org.postgresql:postgresql")
@@ -46,13 +46,13 @@ dependencies {
     testImplementation("com.h2database:h2")
 
     testImplementation("io.kotlintest:kotlintest-assertions:3.4.2")
-    testImplementation("io.rest-assured:rest-assured:4.3.1")
-    testImplementation("com.squareup.okhttp3:okhttp:4.9.0")
-    testImplementation("io.mockk:mockk:1.10.0")
+    testImplementation("io.rest-assured:rest-assured:4.3.3")
+    testImplementation("com.squareup.okhttp3:okhttp:4.9.1")
+    testImplementation("io.mockk:mockk:1.10.5")
     testImplementation("info.novatec.testit:testutils-logrecorder-logback:0.3.4")
-    testImplementation("org.testcontainers:testcontainers:1.14.3")
-    testImplementation("net.jqwik:jqwik:1.3.3")
-    testImplementation("com.tngtech.archunit:archunit:0.14.1")
+    testImplementation("org.testcontainers:testcontainers:1.15.1")
+    testImplementation("net.jqwik:jqwik:1.3.10")
+    testImplementation("com.tngtech.archunit:archunit:0.16.0")
 }
 
 tasks {
