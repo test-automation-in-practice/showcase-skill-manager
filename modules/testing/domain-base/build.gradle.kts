6724409e410e46d3890cfe1bebc597d6b82fe50c
Stefan Ludwig <stefan.ludwig@novatec-gmbh.de>
2021-03-05 23:02:59 +0100

    upgraded rest-assured to finally fix dependency issues

--- modules/testing/domain-base/build.gradle.kts
+++ modules/testing/domain-base/build.gradle.kts
@@ -13,11 +13,9 @@ dependencies {
     api("com.h2database:h2")
     api("com.squareup.okhttp3:okhttp")
     api("com.fasterxml.jackson.module:jackson-module-kotlin")
+    api("io.rest-assured:rest-assured")
 
     api("org.testcontainers:testcontainers:1.15.1")
-    api("io.rest-assured:rest-assured:4.3.3")
-    api("io.rest-assured:json-path:4.3.3")
-    api("io.rest-assured:xml-path:4.3.3")
 
     api("org.springframework.boot:spring-boot-starter-test")
     api("org.springframework.restdocs:spring-restdocs-mockmvc")
