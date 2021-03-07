ae34f07d0932950883e217a753c08fd073e1588d
Stefan Ludwig <stefan.ludwig@novatec-gmbh.de>
2021-03-07 13:24:38 +0100

    generalized eventing integration test configuration

--- modules/testing/domain-base/src/main/kotlin/skillmanagement/test/events/EventingSpringIntegrationTest.kt
+++ modules/testing/domain-base/src/main/kotlin/skillmanagement/test/events/EventingSpringIntegrationTest.kt
@@ -4,16 +4,31 @@ import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry
 import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
 import skillmanagement.common.events.EventsConfiguration
+import skillmanagement.test.TechnologyIntegrationTest
+import skillmanagement.test.e2e.PROPERTY_DOCKERIZED_BROKER_HOST
+import skillmanagement.test.e2e.PROPERTY_DOCKERIZED_BROKER_PORT
+import kotlin.annotation.AnnotationTarget.CLASS
+
+@Retention
+@Target(CLASS)
+@TechnologyIntegrationTest
+@RunWithDockerizedRabbitMq
+@SpringBootTest(
+    classes = [EventingIntegrationTestConfiguration::class],
+    properties = [PROPERTY_DOCKERIZED_BROKER_HOST, PROPERTY_DOCKERIZED_BROKER_PORT]
+)
+annotation class EventingSpringIntegrationTest
 
 @Import(
     EventsConfiguration::class,
     RabbitAutoConfiguration::class,
     JacksonAutoConfiguration::class
 )
-class EventingIntegrationTestConfiguration {
+internal class EventingIntegrationTestConfiguration {
     @Bean
     fun meterRegistry(): MeterRegistry = SimpleMeterRegistry()
 }
