f6b2699ed64dbe235648ea94939a93709a8ee0cc
Stefan Ludwig <stefan.ludwig@novatec-gmbh.de>
2021-03-12 18:22:42 +0100

    partially documented search index interfaces

--- modules/common/domain-base/src/main/kotlin/skillmanagement/common/searchindices/SearchIndex.kt
+++ modules/common/domain-base/src/main/kotlin/skillmanagement/common/searchindices/SearchIndex.kt
@@ -3,11 +3,40 @@ package skillmanagement.common.searchindices
 import skillmanagement.common.model.Suggestion
 import java.util.UUID
 
+/**
+ * Search indices are used to store search related data about entities and make
+ * it possible to query for those entities in a number of ways:
+ *
+ * - paged iteration over all entities
+ * - querying with full _Lucene_ query syntax support
+ * - suggesting relevant entities based on user input
+ *
+ * Note however that a search index will not contain the all of the entity data
+ * and therefore cannot be used on its own. If entity data is needed, the
+ * original data source needs to be queried with the IDs returned by the search
+ * index.
+ */
 interface SearchIndex<T : Any> {
+
+    /**
+     * Create or update an index document for the given [T] instance in the
+     * [SearchIndex].
+     *
+     * Depending on how the [SearchIndex] is configured the change will not
+     * take effect until the regular refresh cycle was executed.
+     */
     fun index(instance: T)
+
+    /**
+     * Deletes the index document with the given ID from the [SearchIndex].
+     *
+     * Depending on how the [SearchIndex] is configured the change will not
+     * take effect until the regular refresh cycle was executed.
+     */
     fun deleteById(id: UUID)
 
     fun query(query: PagedStringQuery): Page<UUID>
     fun findAll(query: PagedFindAllQuery): Page<UUID>
     fun suggest(input: String, max: MaxSuggestions): List<Suggestion>
+
 }
