b0da9c46646c04f655baf1161dc6d49a78699a6e
Stefan Ludwig <stefan.ludwig@novatec-gmbh.de>
2021-03-12 22:03:06 +0100

    more project tests

--- modules/domains/projects/src/test/kotlin/skillmanagement/domain/projects/model/testdata.kt
+++ modules/domains/projects/src/test/kotlin/skillmanagement/domain/projects/model/testdata.kt
@@ -3,11 +3,6 @@ package skillmanagement.domain.projects.model
 import skillmanagement.common.model.Suggestion
 import skillmanagement.test.instant
 import skillmanagement.test.uuid
-import java.time.Instant
-
-// Neo - PS4 Pro
-// Orbis - PS4
-// Morpheus - PS VR
 
 // Example #1
 
@@ -128,24 +123,6 @@ internal val project_change_data_morpheus_json = """
     """.trimIndent()
 
 internal val project_morpheus_suggestion = project_morpheus.toSuggestion()
-
-// Example X
-
-val project_starlink = Project(
-    id = uuid("15c1c1d8-3445-4d95-a945-605aaf087078"),
-    version = 1,
-    label = ProjectLabel("Starlink satellite"),
-    description = ProjectDescription("Lorem ipsum dolor sit amet .."),
-    lastUpdate = Instant.parse("2020-07-14T12:34:56.789Z")
-)
-
-val project_demo_2 = Project(
-    id = uuid("d61c7840-7be3-4668-ae48-6c0b396268b5"),
-    version = 1,
-    label = ProjectLabel("Demo-2"),
-    description = ProjectDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed .."),
-    lastUpdate = Instant.parse("2020-07-14T12:34:56.789Z")
-)
 
 // Functions
 
