a0311887efd22bea66394ade9c80206a7b16449b
Stefan Ludwig <stefan.ludwig@novatec-gmbh.de>
2021-03-13 19:54:33 +0100

    little bit of refactoring

--- modules/domains/skills/src/main/kotlin/skillmanagement/domain/skills/usecases/create/CreateSkillFunction.kt
+++ modules/domains/skills/src/main/kotlin/skillmanagement/domain/skills/usecases/create/CreateSkillFunction.kt
@@ -9,6 +9,7 @@ import skillmanagement.domain.skills.model.SkillDescription
 import skillmanagement.domain.skills.model.SkillLabel
 import skillmanagement.domain.skills.model.Tag
 import java.time.Clock
+import java.util.Collections.emptySortedSet
 import java.util.SortedSet
 
 @BusinessFunction
@@ -19,18 +20,28 @@ class CreateSkillFunction internal constructor(
     private val clock: Clock
 ) {
 
-    operator fun invoke(label: SkillLabel, description: SkillDescription?, tags: SortedSet<Tag>): Skill {
-        val skill = Skill(
-            id = idGenerator.generateId(),
-            version = 1,
-            label = label,
-            description = description,
-            tags = tags,
-            lastUpdate = clock.instant()
-        )
+    operator fun invoke(
+        label: SkillLabel,
+        description: SkillDescription? = null,
+        tags: SortedSet<Tag> = emptySortedSet()
+    ): Skill {
+        val skill = skill(label, description, tags)
         insertSkillIntoDataStore(skill)
         publishEvent(SkillAddedEvent(skill))
         return skill
     }
+
+    private fun skill(
+        label: SkillLabel,
+        description: SkillDescription?,
+        tags: SortedSet<Tag>
+    ) = Skill(
+        id = idGenerator.generateId(),
+        version = 1,
+        label = label,
+        description = description,
+        tags = tags,
+        lastUpdate = clock.instant()
+    )
 
 }
