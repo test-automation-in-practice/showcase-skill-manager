5e631c254de6d4a5f6d3efff7ef50bbb67aeb45b
Stefan Ludwig <stefan.ludwig@novatec-gmbh.de>
2021-03-19 14:59:11 +0100

    implemented admin user in default security
    Authentication can be disabled using the `unsecured` profile

--- runtime/src/main/kotlin/skillmanagement/configuration/WebSecurityConfiguration.kt
+++ runtime/src/main/kotlin/skillmanagement/configuration/WebSecurityConfiguration.kt
@@ -1,27 +1,84 @@
 package skillmanagement.configuration
 
-import org.apache.http.HttpHeaders
-import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.apache.http.HttpHeaders.AUTHORIZATION
+import org.apache.http.HttpHeaders.CACHE_CONTROL
+import org.apache.http.HttpHeaders.CONTENT_TYPE
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest.to
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest.toAnyEndpoint
+import org.springframework.boot.actuate.health.HealthEndpoint
+import org.springframework.boot.actuate.info.InfoEndpoint
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.http.HttpMethod
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy.STATELESS
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.crypto.factory.PasswordEncoderFactories.createDelegatingPasswordEncoder
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
-@EnableWebSecurity
+@Configuration
 class WebSecurityConfiguration(
     val corsProperties: CorsConfigurationProperties
-) : WebSecurityConfigurerAdapter() {
+) {
 
-    override fun configure(http: HttpSecurity) {
-        http.authorizeRequests()
-            .anyRequest().permitAll().and()
-            .csrf().disable()
-            .cors()
+    @Profile("!unsecured")
+    @Configuration
+    @EnableWebSecurity
+    class DefaultWebSecurityConfiguration : WebSecurityConfigurerAdapter() {
+
+        private val encoder = createDelegatingPasswordEncoder();
+
+        override fun configure(http: HttpSecurity): Unit = with(http) {
+            cors()
+            csrf().disable()
+            httpBasic()
+            sessionManagement().sessionCreationPolicy(STATELESS)
+            authorizeRequests { requests ->
+                requests.requestMatchers(to(InfoEndpoint::class.java, HealthEndpoint::class.java)).permitAll()
+                requests.requestMatchers(toAnyEndpoint()).hasRole("ACTUATOR")
+                requests.anyRequest().fullyAuthenticated()
+            }
+        }
+
+        override fun configure(auth: AuthenticationManagerBuilder): Unit = with(auth) {
+            inMemoryAuthentication {
+                withUser(
+                    User.withUsername("admin")
+                        .password(encoder.encode("admin"))
+                        .roles("ADMIN", "ACTUATOR")
+                        .build()
+                )
+            }
+        }
+
+        @Bean
+        override fun authenticationManagerBean(): AuthenticationManager = super.authenticationManagerBean()
+
+        private fun AuthenticationManagerBuilder.inMemoryAuthentication(
+            body: InMemoryUserDetailsManagerConfigurer<*>.() -> Unit
+        ) = body(this.inMemoryAuthentication())
+    }
+
+    @Profile("unsecured")
+    @Configuration
+    @EnableWebSecurity
+    class UnsecuredWebSecurityConfiguration : WebSecurityConfigurerAdapter() {
+
+        override fun configure(http: HttpSecurity): Unit = with(http) {
+            cors()
+            csrf().disable()
+            sessionManagement().sessionCreationPolicy(STATELESS)
+            authorizeRequests().anyRequest().permitAll()
+        }
+
     }
 
     @Bean
@@ -30,13 +87,10 @@ class WebSecurityConfiguration(
         configuration.allowedOrigins = corsProperties.allowedOrigins
         configuration.allowedMethods = HttpMethod.values().map { it.name }
         configuration.allowCredentials = true
-        configuration.allowedHeaders = listOf(
-            HttpHeaders.AUTHORIZATION,
-            HttpHeaders.CACHE_CONTROL,
-            HttpHeaders.CONTENT_TYPE)
+        configuration.allowedHeaders = listOf(AUTHORIZATION, CACHE_CONTROL, CONTENT_TYPE)
         val source = UrlBasedCorsConfigurationSource()
         source.registerCorsConfiguration("/**", configuration)
         return source
     }
-}
 
+}
