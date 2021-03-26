92f3436a34565397b21db67380608f9558d88c59
Stefan Ludwig <stefan.ludwig@novatec-gmbh.de>
2021-03-26 13:31:33 +0100

    tests for gateway code

---
+++ modules/domains/employees/src/test/kotlin/skillmanagement/domain/employees/gateways/GetProjectByIdAdapterFunctionTests.kt
@@ -0,0 +1,44 @@
+package skillmanagement.domain.employees.gateways
+
+import io.kotlintest.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.Test
+import skillmanagement.domain.employees.model.project_neo
+import skillmanagement.domain.projects.model.Project
+import skillmanagement.domain.projects.model.ProjectDescription
+import skillmanagement.domain.projects.model.ProjectLabel
+import skillmanagement.domain.projects.usecases.read.GetProjectByIdFunction
+import skillmanagement.test.ResetMocksAfterEachTest
+import skillmanagement.test.UnitTest
+import skillmanagement.test.instant
+import skillmanagement.test.uuid
+
+@UnitTest
+@ResetMocksAfterEachTest
+internal class GetProjectByIdAdapterFunctionTests {
+
+    private val getProjectById: GetProjectByIdFunction = mockk()
+    private val getProjectByIdAdapter = GetProjectByIdAdapterFunction(getProjectById)
+
+    @Test
+    fun `delegates to function of other module and converts the result`() {
+        val project = Project(
+            id = uuid("f804d83f-466c-4eab-a58f-4b25ca1778f3"),
+            version = 1,
+            label = ProjectLabel("Neo"),
+            description = ProjectDescription("The PlayStation 4 Pro."),
+            lastUpdate = instant("2021-03-11T12:34:56.789Z")
+        )
+        every { getProjectById(project.id) } returns project
+
+        getProjectByIdAdapter(project.id) shouldBe project_neo
+    }
+
+    @Test
+    fun `returns null if there is no project with thte given ID`() {
+        every { getProjectById(any()) } returns null
+        getProjectByIdAdapter(uuid()) shouldBe null
+    }
+
+}
---
+++ modules/domains/employees/src/test/kotlin/skillmanagement/domain/employees/gateways/GetSkillByIdAdapterFunctionTests.kt
@@ -0,0 +1,46 @@
+package skillmanagement.domain.employees.gateways
+
+import io.kotlintest.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.Test
+import skillmanagement.domain.employees.model.skill_kotlin
+import skillmanagement.domain.skills.model.Skill
+import skillmanagement.domain.skills.model.SkillDescription
+import skillmanagement.domain.skills.model.SkillLabel
+import skillmanagement.domain.skills.model.Tag
+import skillmanagement.domain.skills.usecases.read.GetSkillByIdFunction
+import skillmanagement.test.ResetMocksAfterEachTest
+import skillmanagement.test.UnitTest
+import skillmanagement.test.instant
+import skillmanagement.test.uuid
+
+@UnitTest
+@ResetMocksAfterEachTest
+internal class GetSkillByIdAdapterFunctionTests {
+
+    private val getSkillById: GetSkillByIdFunction = mockk()
+    private val getSkillByIdAdapter = GetSkillByIdAdapterFunction(getSkillById)
+
+    @Test
+    fun `delegates to function of other module and converts the result`() {
+        val skill = Skill(
+            id = uuid("3f7985b9-f5f0-4662-bda9-1dcde01f5f3b"),
+            version = 1,
+            label = SkillLabel("Kotlin"),
+            description = SkillDescription("The coolest programming language."),
+            tags = sortedSetOf(Tag("language"), Tag("cool")),
+            lastUpdate = instant("2020-07-14T12:34:56.789Z")
+        )
+        every { getSkillById(skill.id) } returns skill
+
+        getSkillByIdAdapter(skill.id) shouldBe skill_kotlin
+    }
+
+    @Test
+    fun `returns null if there is no project with thte given ID`() {
+        every { getSkillById(any()) } returns null
+        getSkillByIdAdapter(uuid()) shouldBe null
+    }
+
+}
