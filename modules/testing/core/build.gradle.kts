90288fd1f76cc05464b67a85883406442732c0e2
Stefan Ludwig <stefan.ludwig@novatec-gmbh.de>
2021-04-01 17:11:06 +0200

    remove incompatible dependency

--- modules/testing/core/build.gradle.kts
+++ modules/testing/core/build.gradle.kts
@@ -11,7 +11,9 @@ dependencies {
     api("net.jqwik:jqwik:1.3.10")
     api("io.mockk:mockk:1.10.5")
     api("org.assertj:assertj-core")
-    api("io.kotlintest:kotlintest-assertions:3.4.2")
+    api("io.kotlintest:kotlintest-assertions:3.4.2") {
+        exclude(group = "io.arrow-kt")
+    }
     api("info.novatec.testit:testutils-logrecorder-logback:0.3.4")
     api("com.tngtech.archunit:archunit:0.16.0")
 
