0c16f132a317f95693b7fd17e77091ee1be55368
Stefan Ludwig <stefan.ludwig@novatec-gmbh.de>
2021-05-07 12:05:23 +0200

    add missing project test

--- modules/domains/projects/src/test/kotlin/skillmanagement/domain/projects/model/ProjectEntityTests.kt
+++ modules/domains/projects/src/test/kotlin/skillmanagement/domain/projects/model/ProjectEntityTests.kt
@@ -1,10 +1,36 @@
 package skillmanagement.domain.projects.model
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
 import skillmanagement.test.AbstractJsonSerializationTests
 import skillmanagement.test.TechnologyIntegrationTest
+import skillmanagement.test.UnitTest
 
 internal class ProjectEntityTests {
+
+    @Nested
+    @UnitTest
+    inner class FunctionTests {
+
+        @Test
+        fun `update replaces project data`() {
+            val actual = project_neo.update { skill ->
+                skill.copy(
+                    label = ProjectLabel("updated label"),
+                    description = ProjectDescription("updated description")
+                )
+            }
+            val expected = project_neo.copy(
+                data = Project(
+                    label = ProjectLabel("updated label"),
+                    description = ProjectDescription("updated description")
+                )
+            )
+            assertThat(actual).isEqualTo(expected)
+        }
+
+    }
 
     @Nested
     @TechnologyIntegrationTest
